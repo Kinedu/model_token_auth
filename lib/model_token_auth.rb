9323e13122ce82593c408b80c378a8c0c9778ebc
Armando Alejandre <armando@kinedu.com>
2019-05-29 17:03:53 -0500

    acts_as_controller_authenticable methods was changed

--- lib/model_token_auth.rb
+++ lib/model_token_auth.rb
@@ -3,20 +3,21 @@ require 'model_token_auth/access_tokens_config'
 require 'model_token_auth/acts_as_model_authenticable'
 require 'model_token_auth/acts_as_controllers_authenticable'
 
-module ModelTokenAuth; end
-
+# => Class associated with the models
+# that will be authenticated.
+#
+# if you want to add functionality or
+# parameters you need to open the class
+# in the models folder of the rails application
+#
 class AccessToken < ActiveRecord::Base
   include ModelTokenAuth::AccessTokensConfig
 end
 
-ActiveRecord::Base.include(
-  ModelTokenAuth::ActsAsModelAuthenticable
-)
+# => *
+ActiveRecord::Base.include ModelTokenAuth::ActsAsModelAuthenticable
 
-ActionController::Base.include(
-  ModelTokenAuth::ActsAsControllersAuthenticable
-) if ActionController::Base
-
-ActionController::API.include(
-  ModelTokenAuth::ActsAsControllersAuthenticable
-) if ActionController::API
+# => *
+[ActionController::Base, ActionController::API].each do |resource|
+  resource.include ModelTokenAuth::ActsAsControllersAuthenticable
+end
